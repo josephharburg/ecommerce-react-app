85afb81a8bc0715aa63936378273698889ed4065
Joseph Harburg <josephharburg@gmail.com>
2021-11-11 12:24:51 -0500

    moved directory and shop data into its own reducers

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -4,7 +4,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   if(existingCartItem){
     return cartItems.map(cartItem =>
-      cartItem.id == cartItemToAdd.id
+      cartItem.id === cartItemToAdd.id
       ? {...cartItem, quantity: cartItem.quantity + 1}
       :      cartItem
     )
@@ -17,12 +17,12 @@ export const removeItemFromCart = (cartItems, cartItemToRemove) => {
   const existingCartItem = cartItems.find(
     cartItem => cartItem.id === cartItemToRemove.id);
 
-    if(existingCartItem.quantity == 1){
+    if(existingCartItem.quantity === 1){
       return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
     }
 
     return cartItems.map(
-      cartItem => cartItem.id == cartItemToRemove.id ?
+      cartItem => cartItem.id === cartItemToRemove.id ?
       {...cartItem, quantity: cartItem.quantity - 1}:
       cartItem
     )
